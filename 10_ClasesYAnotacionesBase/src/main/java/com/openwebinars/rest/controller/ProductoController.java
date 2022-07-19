3ee952546d89275cccfbfdd178a0736e432a79c9
edhydev <edgar.jimenez.dev@gmail.com>
2022-07-19 17:08:27 -0500

    Corrección de códigos HTTP

--- 10_ClasesYAnotacionesBase/src/main/java/com/openwebinars/rest/controller/ProductoController.java
+++ 10_ClasesYAnotacionesBase/src/main/java/com/openwebinars/rest/controller/ProductoController.java
@@ -1,87 +1,91 @@
 package com.openwebinars.rest.controller;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.openwebinars.rest.modelo.Producto;
 import com.openwebinars.rest.modelo.ProductoRepositorio;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ProductoController {
 
-	private final ProductoRepositorio productoRepositorio;
+    private final ProductoRepositorio productoRepositorio;
 
-	/**
-	 * Obtenemos todos los productos
-	 * 
-	 * @return
-	 */
-	@GetMapping("/producto")
-	public List<Producto> obtenerTodos() {
-		return productoRepositorio.findAll();
-	}
+    /**
+     * Obtenemos todos los productos
+     *
+     * @return
+     */
+    @GetMapping("/producto")
+    public ResponseEntity<?> obtenerTodos() {
+        List<Producto> result = productoRepositorio.findAll();
 
-	/**
-	 * Obtenemos un producto en base a su ID
-	 * 
-	 * @param id
-	 * @return Null si no encuentra el producto
-	 */
-	@GetMapping("/producto/{id}")
-	public Producto obtenerUno(@PathVariable Long id) {
-		return productoRepositorio.findById(id).orElse(null);
-	}
+        if (result.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
 
-	/**
-	 * Insertamos un nuevo producto
-	 * 
-	 * @param nuevo
-	 * @return producto insertado
-	 */
-	@PostMapping("/producto")
-	public Producto nuevoProducto(@RequestBody Producto nuevo) {
-		return productoRepositorio.save(nuevo);
-	}
+    /**
+     * Obtenemos un producto en base a su ID
+     *
+     * @param id
+     * @return Null si no encuentra el producto
+     */
+    @GetMapping("/producto/{id}")
+    public ResponseEntity<?> obtenerUno(@PathVariable Long id) {
+        Producto result = productoRepositorio.findById(id).orElse(null);
 
-	/**
-	 * 
-	 * @param editar
-	 * @param id
-	 * @return
-	 */
-	@PutMapping("/producto/{id}")
-	public Producto editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
-		if (productoRepositorio.existsById(id)) {
-			editar.setId(id);
-			return productoRepositorio.save(editar);
-		} else {
-			return null;
-		}
-	}
+        if (result == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(result);
+        }
+    }
 
-	/**
-	 * Borra un producto del catálogo en base a su id
-	 * @param id
-	 * @return
-	 */
-	@DeleteMapping("/producto/{id}")
-	public Producto borrarProducto(@PathVariable Long id) {
-		if (productoRepositorio.existsById(id)) {
-			Producto result = productoRepositorio.findById(id).get();
-			productoRepositorio.deleteById(id);
-			return result;
-		} else
-			return null;
-	}
+    /**
+     * Insertamos un nuevo producto
+     *
+     * @param nuevo
+     * @return producto insertado
+     */
+    @PostMapping("/producto")
+    public ResponseEntity<Producto> nuevoProducto(@RequestBody Producto nuevo) {
+        Producto saved = productoRepositorio.save(nuevo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    /**
+     * @param editar
+     * @param id
+     * @return
+     */
+    @PutMapping("/producto/{id}")
+    public ResponseEntity<?> editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+        return productoRepositorio.findById(id).map(p -> {
+            p.setNombre(editar.getNombre());
+            p.setPrecio(editar.getPrecio());
+            return ResponseEntity.ok(productoRepositorio.save(p));
+        }).orElseGet(() -> {
+            return ResponseEntity.notFound().build();
+        });
+    }
+
+    /**
+     * Borra un producto del catálogo en base a su id
+     *
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/producto/{id}")
+    public ResponseEntity<?> borrarProducto(@PathVariable Long id) {
+        productoRepositorio.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
