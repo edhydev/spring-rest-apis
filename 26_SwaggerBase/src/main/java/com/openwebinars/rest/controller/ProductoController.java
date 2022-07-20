7d5c3e08316f3ea779a40d322c756db49b2c4616
edhydev <edgar.jimenez.dev@gmail.com>
2022-07-20 11:31:23 -0500

    Se implementa Swagger para documentar la API REST

--- 26_SwaggerBase/src/main/java/com/openwebinars/rest/controller/ProductoController.java
+++ 26_SwaggerBase/src/main/java/com/openwebinars/rest/controller/ProductoController.java
@@ -3,6 +3,11 @@ package com.openwebinars.rest.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.openwebinars.rest.error.ApiError;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -34,113 +39,119 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductoController {
 
-	private final ProductoRepositorio productoRepositorio;
-	private final CategoriaRepositorio categoriaRepositorio;
-	private final ProductoDTOConverter productoDTOConverter;
-	private final StorageService storageService;
+  private final ProductoRepositorio productoRepositorio;
+  private final CategoriaRepositorio categoriaRepositorio;
+  private final ProductoDTOConverter productoDTOConverter;
+  private final StorageService storageService;
 
-	/**
-	 * Obtenemos todos los productos
-	 * 
-	 * @return 404 si no hay productos, 200 y lista de productos si hay uno o más
-	 */
-	@GetMapping("/producto")
-	public ResponseEntity<?> obtenerTodos() {
-		List<Producto> result = productoRepositorio.findAll();
+  /**
+   * Obtenemos todos los productos
+   *
+   * @return 404 si no hay productos, 200 y lista de productos si hay uno o más
+   */
+  @GetMapping("/producto")
+  public ResponseEntity<?> obtenerTodos() {
+    List<Producto> result = productoRepositorio.findAll();
 
-		if (result.isEmpty()) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No hay productos registrados");
-		} else {
+    if (result.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No hay productos registrados");
+    } else {
 
-			List<ProductoDTO> dtoList = result.stream().map(productoDTOConverter::convertToDto)
-					.collect(Collectors.toList());
+      List<ProductoDTO> dtoList = result.stream().map(productoDTOConverter::convertToDto)
+          .collect(Collectors.toList());
 
-			return ResponseEntity.ok(dtoList);
-		}
+      return ResponseEntity.ok(dtoList);
+    }
 
-	}
+  }
 
-	/**
-	 * Obtenemos un producto en base a su ID
-	 * 
-	 * @param id
-	 * @return 404 si no encuentra el producto, 200 y el producto si lo encuentra
-	 */
-	@GetMapping("/producto/{id}")
-	public Producto obtenerUno(@PathVariable Long id) {
-		try {
-			return productoRepositorio.findById(id)
-					.orElseThrow(() -> new ProductoNotFoundException(id));
-		} catch (ProductoNotFoundException ex) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
-		}
-				
-	}
+  /**
+   * Obtenemos un producto en base a su ID
+   *
+   * @param id
+   * @return 404 si no encuentra el producto, 200 y el producto si lo encuentra
+   */
+  @ApiOperation(value = "Obtener un producto por su ID", notes = "Provee un mecanismo para obtener todos los datos de un producto por su ID")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "OK", response = Producto.class),
+      @ApiResponse(code = 404, message = "Not Found", response = ApiError.class),
+      @ApiResponse(code = 500, message = "Internal Server Error", response = ApiError.class),
+  })
+  @GetMapping("/producto/{id}")
+  public Producto obtenerUno(@ApiParam(value = "ID del producto", required = true, type = "long") @PathVariable Long id) {
+    try {
+      return productoRepositorio.findById(id)
+          .orElseThrow(() -> new ProductoNotFoundException(id));
+    } catch (ProductoNotFoundException ex) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
 
-	/**
-	 * Insertamos un nuevo producto
-	 * 
-	 * @param nuevo
-	 * @return 201 y el producto insertado
-	 */
-	@PostMapping(value = "/producto", consumes= MediaType.MULTIPART_FORM_DATA_VALUE) //Aunque no es obligatorio, podemos indicar que se consume multipart/form-data
-	public ResponseEntity<?> nuevoProducto(@RequestPart("nuevo") CreateProductoDTO nuevo, 
-			@RequestPart("file") MultipartFile file) {
-		
-		// Almacenamos el fichero y obtenemos su URL
-		String urlImagen = null;
-		
-		if (!file.isEmpty()) {
-			String imagen = storageService.store(file);
-			urlImagen = MvcUriComponentsBuilder
-							// El segundo argumento es necesario solo cuando queremos obtener la imagen
-							// En este caso tan solo necesitamos obtener la URL
-							.fromMethodName(FicherosController.class, "serveFile", imagen, null)  
-							.build().toUriString();
-		}
-		
-		// Construimos nuestro nuevo Producto a partir del DTO
-		// Como decíamos en ejemplos anteriores, esto podría ser más bien código
-		// de un servicio, pero lo dejamos aquí para no hacer más complejo el código.
-		Producto nuevoProducto = new Producto();
-		nuevoProducto.setNombre(nuevo.getNombre());
-		nuevoProducto.setPrecio(nuevo.getPrecio());
-		nuevoProducto.setImagen(urlImagen);
-		Categoria categoria = categoriaRepositorio.findById(nuevo.getCategoriaId()).orElse(null);
-		nuevoProducto.setCategoria(categoria);
-		return ResponseEntity.status(HttpStatus.CREATED).body(productoRepositorio.save(nuevoProducto));
-	}
+  }
 
-	/**
-	 * 
-	 * @param editar
-	 * @param id
-	 * @return 200 Ok si la edición tiene éxito, 404 si no se encuentra el producto
-	 */
-	@PutMapping("/producto/{id}")
-	public Producto editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+  /**
+   * Insertamos un nuevo producto
+   *
+   * @param nuevo
+   * @return 201 y el producto insertado
+   */
+  @PostMapping(value = "/producto", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  //Aunque no es obligatorio, podemos indicar que se consume multipart/form-data
+  public ResponseEntity<?> nuevoProducto(@RequestPart("nuevo") CreateProductoDTO nuevo,
+                                         @RequestPart("file") MultipartFile file) {
 
-		return productoRepositorio.findById(id).map(p -> {
-			p.setNombre(editar.getNombre());
-			p.setPrecio(editar.getPrecio());
-			return productoRepositorio.save(p);
-		}).orElseThrow(() -> new ProductoNotFoundException(id));
-	}
+    // Almacenamos el fichero y obtenemos su URL
+    String urlImagen = null;
 
-	/**
-	 * Borra un producto del catálogo en base a su id
-	 * 
-	 * @param id
-	 * @return Código 204 sin contenido
-	 */
-	@DeleteMapping("/producto/{id}")
-	public ResponseEntity<?> borrarProducto(@PathVariable Long id) {
-		Producto producto = productoRepositorio.findById(id)
-				.orElseThrow(() -> new ProductoNotFoundException(id));
-		
-		productoRepositorio.delete(producto);
-		return ResponseEntity.noContent().build();
-	}
-	
+    if (!file.isEmpty()) {
+      String imagen = storageService.store(file);
+      urlImagen = MvcUriComponentsBuilder
+          // El segundo argumento es necesario solo cuando queremos obtener la imagen
+          // En este caso tan solo necesitamos obtener la URL
+          .fromMethodName(FicherosController.class, "serveFile", imagen, null)
+          .build().toUriString();
+    }
+
+    // Construimos nuestro nuevo Producto a partir del DTO
+    // Como decíamos en ejemplos anteriores, esto podría ser más bien código
+    // de un servicio, pero lo dejamos aquí para no hacer más complejo el código.
+    Producto nuevoProducto = new Producto();
+    nuevoProducto.setNombre(nuevo.getNombre());
+    nuevoProducto.setPrecio(nuevo.getPrecio());
+    nuevoProducto.setImagen(urlImagen);
+    Categoria categoria = categoriaRepositorio.findById(nuevo.getCategoriaId()).orElse(null);
+    nuevoProducto.setCategoria(categoria);
+    return ResponseEntity.status(HttpStatus.CREATED).body(productoRepositorio.save(nuevoProducto));
+  }
+
+  /**
+   * @param editar
+   * @param id
+   * @return 200 Ok si la edición tiene éxito, 404 si no se encuentra el producto
+   */
+  @PutMapping("/producto/{id}")
+  public Producto editarProducto(@RequestBody Producto editar, @PathVariable Long id) {
+
+    return productoRepositorio.findById(id).map(p -> {
+      p.setNombre(editar.getNombre());
+      p.setPrecio(editar.getPrecio());
+      return productoRepositorio.save(p);
+    }).orElseThrow(() -> new ProductoNotFoundException(id));
+  }
+
+  /**
+   * Borra un producto del catálogo en base a su id
+   *
+   * @param id
+   * @return Código 204 sin contenido
+   */
+  @DeleteMapping("/producto/{id}")
+  public ResponseEntity<?> borrarProducto(@PathVariable Long id) {
+    Producto producto = productoRepositorio.findById(id)
+        .orElseThrow(() -> new ProductoNotFoundException(id));
+
+    productoRepositorio.delete(producto);
+    return ResponseEntity.noContent().build();
+  }
+
 
 }
--- 26_SwaggerBase/src/main/java/com/openwebinars/rest/modelo/Producto.java
+++ 26_SwaggerBase/src/main/java/com/openwebinars/rest/modelo/Producto.java
@@ -6,25 +6,34 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Producto {
 
-	@Id @GeneratedValue
-	private Long id;
-	
-	private String nombre;
-	
-	private float precio;
-	
-	private String imagen;
-	
-	@ManyToOne
-	@JoinColumn(name="categoria_id")
-	private Categoria categoria;
-	
+  @ApiModelProperty(value = "ID del producto", dataType = "long", example = "1", position = 1)
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ApiModelProperty(value = "Nombre del producto", dataType = "String", example = "Peñafiel Piñada 2L", position = 2)
+  private String nombre;
+
+  @ApiModelProperty(value = "Precio del producto", dataType = "float", example = "28.50", position = 3)
+  private float precio;
+
+  @ApiModelProperty(value = "Imagen del producto", dataType = "String", example = "http://www.midominio.com/files/12345-imagen.jpg", position = 4)
+  private String imagen;
+
+  @ApiModelProperty(value = "Categoría del producto", dataType = "Categoria", position = 5)
+  @ManyToOne
+  @JoinColumn(name = "categoria_id")
+  private Categoria categoria;
+
 }
