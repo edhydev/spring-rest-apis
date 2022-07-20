ecbbd3971c4a2a6d1d02a5dd72997fbf53f568a3
edhydev <edgar.jimenez.dev@gmail.com>
2022-07-20 10:20:36 -0500

    Se implementa el sistema de subida de archivos

--- 23_UploadBase/src/main/java/com/openwebinars/rest/Application.java
+++ 23_UploadBase/src/main/java/com/openwebinars/rest/Application.java
@@ -1,28 +1,24 @@
 package com.openwebinars.rest;
 
+import com.openwebinars.rest.upload.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.openwebinars.rest.upload.StorageService;
-
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
-	@Bean
-	public CommandLineRunner init(StorageService storageService) {
-		return args -> {
-			// Inicializamos el servicio de ficheros
-			storageService.deleteAll();
-			storageService.init();
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
-		};
-
-	}
-
+  @Bean
+  public CommandLineRunner init(StorageService storageService) {
+    return args -> {
+      // Inicializamos el servicio de ficheros
+      storageService.deleteAll();
+      storageService.init();
+    };
+  }
 }
