0c349cbea18ca98289f1aebe37f9547b9d5c266a
edhydev <edgar.jimenez.dev@gmail.com>
2022-07-20 09:52:15 -0500

    Se implementa las cors a nivel global

--- 21_CORSGlobalBase/src/main/java/com/openwebinars/rest/config/MiConfiguracion.java
+++ 21_CORSGlobalBase/src/main/java/com/openwebinars/rest/config/MiConfiguracion.java
@@ -3,13 +3,30 @@ package com.openwebinars.rest.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MiConfiguracion {
-	
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        //registry.addMapping("/**");
+        registry.addMapping("/producto/**")
+            .allowedOrigins("http://localhost:9001")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .maxAge(3600);
+        ;
+      }
+    };
+  }
 
 }
